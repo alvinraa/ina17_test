0fb1085023faa3163e3e88f0f32e91457a04f8c0
Alvin Ramadhani <alvin080297@gmail.com>
2024-09-07 16:20:34 +0700

    setup base project

---
+++ lib/core/common/routes.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:ina17_test/feature/home/presentation/home_page.dart';
+import 'package:ina17_test/feature/testing/testing_page.dart';
+
+class Routes {
+  static const String homePage = '/';
+  static const String testingPage = 'testing';
+}
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case Routes.testingPage:
+      return MaterialPageRoute(
+        builder: (BuildContext context) => const TestingPage(),
+        settings: settings,
+      );
+    case Routes.homePage:
+      return MaterialPageRoute(
+        builder: (BuildContext context) => const HomePage(),
+        settings: settings,
+      );
+    default:
+      return MaterialPageRoute(
+        builder: (BuildContext context) => const Scaffold(
+          body: Center(
+            child: Text('Route not defined'),
+          ),
+        ),
+        settings: settings,
+      );
+  }
+}
