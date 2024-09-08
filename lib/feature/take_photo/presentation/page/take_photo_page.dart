4a18faad6610fe71739b75f05b1ca899489bc2fc
Alvin Ramadhani <alvin080297@gmail.com>
2024-09-08 13:33:38 +0700

    integrate image to text in take_photo_page
    - add dependencies image_picker and google_mlkit_text_recognition
    - create function getImage for take image
    - create function performceTextRecognition to extract image into text, and then call calculate function to calculating the result
    - create calc function
    - improve UI

--- lib/feature/take_photo/presentation/page/take_photo_page.dart
+++ lib/feature/take_photo/presentation/page/take_photo_page.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:ina17_test/core/common/logger.dart';
 import 'package:ina17_test/core/widget/button/default_button.dart';
 
@@ -10,6 +15,85 @@ class TakePhotoPage extends StatefulWidget {
 }
 
 class _TakePhotoPageState extends State<TakePhotoPage> {
+  XFile? pickedImage;
+  String textFromImage = '';
+  bool scanning = false;
+  int calcResult = 0;
+
+  final ImagePicker _imagePicker = ImagePicker();
+
+  getImage(ImageSource imageSource) async {
+    XFile? result = await _imagePicker.pickImage(
+      source: imageSource,
+      preferredCameraDevice: CameraDevice.rear,
+    );
+
+    if (result != null) {
+      setState(() {
+        pickedImage = result;
+      });
+
+      performceTextRecognition();
+    }
+  }
+
+  performceTextRecognition() async {
+    setState(() {
+      scanning = true;
+    });
+    try {
+      if (pickedImage?.path != null) {
+        // add textRecognizer
+        final textRecognizer =
+            TextRecognizer(script: TextRecognitionScript.latin);
+        // add image from path
+        final InputImage inputImage =
+            InputImage.fromFilePath(pickedImage?.path ?? '');
+        // process the image
+        final RecognizedText recognizedText =
+            await textRecognizer.processImage(inputImage);
+
+        setState(() {
+          textFromImage = recognizedText.text;
+          calculate(textFromImage);
+          scanning = false;
+        });
+
+        textRecognizer.close();
+      }
+    } catch (e) {
+      Logger.print('error during text recognition : $e');
+    }
+  }
+
+  // for now only support single calculation
+  // example : 1+1, 2-2, 3*3, 4:2
+  int calculate(String s) {
+    // Split string based on the operators
+    List<String> numbers = s.split(RegExp(r'[\+\-\*\/]'));
+
+    // Get the operator
+    String operator = s.replaceAll(RegExp(r'[0-9]'), '');
+
+    // Convert string numbers to integers
+    int num1 = int.parse(numbers[0]);
+    int num2 = int.parse(numbers[1]);
+
+    // Perform the operation based on the operator
+    switch (operator) {
+      case '+':
+        return calcResult = num1 + num2;
+      case '-':
+        return calcResult = num1 - num2;
+      case '*':
+        return calcResult = num1 * num2;
+      case '/':
+        return calcResult = num1 ~/ num2; // pembagian Integer
+      default:
+        throw Exception('Invalid operator');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,8 +109,8 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
   }
 
   Widget buildContent(BuildContext context) {
-    // var textTheme = Theme.of(context).textTheme;
-    // var colorScheme = Theme.of(context).colorScheme;
+    var textTheme = Theme.of(context).textTheme;
+    var colorScheme = Theme.of(context).colorScheme;
 
     return Column(
       children: [
@@ -36,7 +120,91 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
             physics: const ClampingScrollPhysics(),
             children: [
               // will change to image from galery or camera
-              Image.network('https://picsum.photos/250?image=9'),
+              pickedImage == null
+                  ? Container(
+                      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: colorScheme.onPrimary,
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      height: 300,
+                      child: Center(
+                        child: Text(
+                          'No image selected',
+                          softWrap: true,
+                          style: GoogleFonts.lato(
+                            textStyle: textTheme.labelLarge?.copyWith(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: colorScheme.secondary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container(
+                      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: colorScheme.onPrimary,
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      height: 300,
+                      child: Center(
+                        child: Image.file(
+                          File(pickedImage?.path ?? ''),
+                          height: 300,
+                        ),
+                      ),
+                    ),
+              const SizedBox(height: 20),
+              Center(
+                child: Text(
+                  'Recognized Text : ',
+                  softWrap: true,
+                  style: GoogleFonts.lato(
+                    textStyle: textTheme.labelLarge?.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ),
+              pickedImage == null
+                  ? Container()
+                  : Container(
+                      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 24,
+                      ),
+                      decoration: BoxDecoration(
+                        color: colorScheme.onPrimary,
+                        border: Border.all(color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Text(
+                        '$textFromImage = $calcResult',
+                        softWrap: true,
+                        style: GoogleFonts.lato(
+                          textStyle: textTheme.labelLarge?.copyWith(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: colorScheme.secondary,
+                          ),
+                        ),
+                      ),
+                    )
             ],
           ),
         ),
@@ -56,15 +224,31 @@ class _TakePhotoPageState extends State<TakePhotoPage> {
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8),
-          child: DefaultButton(
-            margin: const EdgeInsets.symmetric(horizontal: 16),
-            onPressed: () {
-              // go to add input page
-              Logger.print('take photo');
-            },
-            // showLoading: state is SelfSummarySaveLoading,
-            label: 'Take Photo',
-            height: 40,
+          child: Row(
+            children: [
+              Expanded(
+                child: DefaultButton(
+                  margin: const EdgeInsets.symmetric(horizontal: 8),
+                  onPressed: () {
+                    getImage(ImageSource.camera);
+                  },
+                  // showLoading: state is SelfSummarySaveLoading,
+                  label: 'Take a picture',
+                  height: 40,
+                ),
+              ),
+              Expanded(
+                child: DefaultButton(
+                  margin: const EdgeInsets.symmetric(horizontal: 8),
+                  onPressed: () {
+                    getImage(ImageSource.gallery);
+                  },
+                  // showLoading: state is SelfSummarySaveLoading,
+                  label: 'Select from galery',
+                  height: 40,
+                ),
+              ),
+            ],
           ),
         ),
       ),
